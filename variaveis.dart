4491521a5c70d4f81886fd0a21799cabbc6171f9
Alef Thiago da Silva <alefsilvadn@gmail.com>
2024-07-22 14:15:48 -0300

    primeiros ex

---
+++ variaveis.dart
@@ -0,0 +1,7 @@
+void main() {
+//      Implemente um programa capaz computar a média ponderada entre três números. Fórmula da média ponderada.     //
+    int valor1 = 4, valor2 = 6, valor3 = 10, peso1 = 1, peso2 = 2, peso3 = 3;
+    double media_ponderada = ((valor1 * peso1) + (valor2 * peso2) + (valor3 * peso3)) / (peso1 + peso2 + peso3);
+    print('A media ponderada eh ${media_ponderada}');
+//     /Implemente um programa capaz computar a média ponderada entre três números. Fórmula da média ponderada.     //
+}
